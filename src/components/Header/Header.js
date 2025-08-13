0fa35fce124a279895033bf55d722bf6e427915b
gabor6502 <elan.gabor@gmail.com>
2025-08-13 15:50:02 -0500

    moved hover/clickable styles from header to index

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,10 +7,10 @@ export default function Header()
     return(<>
         <nav id="header" className="text-center">
             <span id="links">
-                <Link class="undecLink" to="/"><span className="fs-1 fw-bold">Elan Gabor</span></Link>
-                <Link class="undecLink" to="/projects"><span>Projects</span></Link>
-                <Link class ="undecLink" to="/about"><span>About</span></Link>
-                <Link class="undecLink" to="/contact"><span>Contact</span></Link>
+                <Link class="undecLink" to="/"><span className="fs-1 fw-bold clickable">Elan Gabor</span></Link>
+                <Link class="undecLink" to="/projects"><span className="clickable">Projects</span></Link>
+                <Link class ="undecLink" to="/about"><span className="clickable">About</span></Link>
+                <Link class="undecLink" to="/contact"><span className="clickable">Contact</span></Link>
             </span>
         </nav>
     </>)
