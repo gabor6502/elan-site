653d4a905a949e305c49bc80965cc4de5636f6c7
gabor6502 <elan.gabor@gmail.com>
2025-08-13 12:44:22 -0500

    gradient and blurred header bar setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,17 @@
 import { Link } from "react-router"
-import { Row, Col, Container } from "react-bootstrap"
 
 import "./header.css"
 
 export default function Header()
 {
     return(<>
-        <Container id="header">
-            <h1><i><Link class="undecLink" to="/"><span>Elan Gabor</span></Link></i></h1>
-            <nav>
-                <Row >
-                    <span id="links">
-                        <Link class="undecLink" to="/projects"><span>Projects</span></Link>
-                        <Link class ="undecLink" to="/about"><span>About</span></Link>
-                        <Link class="undecLink" to="/contact"><span>Contact</span></Link>
-                    </span>
-                </Row>
-            </nav>
-        </Container>
+        <nav id="header" className="text-center">
+            <span id="links">
+                <Link class="undecLink" to="/"><span className="fs-1"><b>Elan Gabor</b></span></Link>
+                <Link class="undecLink" to="/projects"><span>Projects</span></Link>
+                <Link class ="undecLink" to="/about"><span>About</span></Link>
+                <Link class="undecLink" to="/contact"><span>Contact</span></Link>
+            </span>
+        </nav>
     </>)
 }
