a4bbefbe5048e24bc8a850a3dc3b371502ac4c61
gabor6502 <elan.gabor@gmail.com>
2025-08-13 14:39:17 -0500

    transitions for header item hover

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ export default function Header()
     return(<>
         <nav id="header" className="text-center">
             <span id="links">
-                <Link class="undecLink" to="/"><span className="fs-1"><b>Elan Gabor</b></span></Link>
+                <Link class="undecLink" to="/"><span className="fs-1">Elan Gabor</span></Link>
                 <Link class="undecLink" to="/projects"><span>Projects</span></Link>
                 <Link class ="undecLink" to="/about"><span>About</span></Link>
                 <Link class="undecLink" to="/contact"><span>Contact</span></Link>
