1fced027dd485ae0ce00274f809f6b6249c30ccc
gabor6502 <elan.gabor@gmail.com>
2025-09-09 11:04:43 -0500

    adding repo buttons, starting with animation

---
+++ src/components/Projects/CodeLinkButton.js
@@ -0,0 +1,9 @@
+
+export default function CodeLinkButton({name, link})
+{
+    return (<>
+        <span className={"repoLinkButton"}>
+            {name}
+        </span>
+    </>)
+}
--- src/components/Projects/CodeLinks.js
+++ src/components/Projects/CodeLinks.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import { Image } from "react-bootstrap"
+import CodeLinkButton from "./CodeLinkButton"
 
 export default function CodeLinks({linkSet})
 {
@@ -8,15 +10,22 @@ export default function CodeLinks({linkSet})
     {
         setLinks(linkSet.map((linkObj) => 
         {
-            return {linkName: Object.keys(linkObj)[0], link: linkObj.link}
+            return {name: Object.keys(linkObj)[0], url: linkObj.link}
         }))
     }, [linkSet, links])
 
+  
+
     return (<>
-        <span>
-            {
-                links.map((linkObj) => { return <p>{linkObj.linkName}</p>})
-            }
-        </span>
+        <div>
+            <span className="repoLinksContainer">
+                <Image className={"repobadge"} src={require(`../../resources/pictures/git.png`)} alt={"repo logo"} />
+                    <span className={"overflowControl"}>
+                        {
+                        links.map((link) => { return <CodeLinkButton name={link.name} link={link.url}/>})
+                        }
+                    </span>
+            </span>
+        </div>
     </>)
 }
