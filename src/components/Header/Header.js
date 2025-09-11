78786034c3a7a208fa3d04123811ac0730065d40
gabor6502 <elan.gabor@gmail.com>
2025-09-11 11:14:48 -0500

    make clicked border bigger on header links but other than that header re-layout done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 import { Link, useLocation  } from "react-router"
 import { Container, Image, Row, Col } from "react-bootstrap"
 
-import "./header.css"
 
 function setClicked(pathname, linkto)
 {
@@ -44,13 +43,12 @@ export default function Header()
                                 pagelinks.map((link) => 
                                 {
                                     return (<><Link className="undecLink" to={link.to}>
-                                                <span className={" headerClickable " + setClicked(currPath, link.to)}>
+                                                <span className={"fs-5 fw-bold headerClickable " + setClicked(currPath, link.to)}>
                                                     {link.content}
                                                 </span>
                                             </Link></>)
                                 })
                             }
-
                             <Image id="linkedIn" rounded src={require("../../resources/pictures/linkedinIcon.ico")} onClick={openLinkedInProfile} title="Open LinkedIn profile" alt="LinkedIn logo" />
                             <Image id="GitHub" rounded src={require("../../resources/pictures/githubIcon.ico")} onClick={openGitHubProfile} title="Open GitHub profile" alt="GitHub logo" />
                         </span>
