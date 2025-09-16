823d86d8795c85566f52e0f651d661b9043ba3f3
gabor6502 <elan.gabor@gmail.com>
2025-09-16 09:14:30 -0500

    hash routed links

--- src/components/Header/HeaderRoutes.js
+++ src/components/Header/HeaderRoutes.js
@@ -8,9 +8,9 @@ export default function HeaderRoutes()
 {
     return (<>
         <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/contact" element={<Contact /> } />
+            <Route exact path="" element={<Home />} />
+            <Route exact path="about" element={<About />} />
+            <Route exact path="contact" element={<Contact /> } />
         </Routes>
     </>)
 }
