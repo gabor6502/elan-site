c4622467e25a5d7f1402c3d238d046603fe50ecf
gabor6502 <elan.gabor@gmail.com>
2025-09-16 01:04:07 -0500

    attempt push dispatch and url fix

--- src/components/Header/HeaderRoutes.js
+++ src/components/Header/HeaderRoutes.js
@@ -8,9 +8,9 @@ export default function HeaderRoutes()
 {
     return (<>
         <Routes>
-            <Route exact path="elan-site/" element={<Home />} />
-            <Route exact path="elan-site/about" element={<About />} />
-            <Route exact path="elan-site/contact" element={<Contact /> } />
+            <Route exact path="/elan-site/" element={<Home />} />
+            <Route exact path="/elan-site/about" element={<About />} />
+            <Route exact path="/elan-site/contact" element={<Contact /> } />
         </Routes>
     </>)
 }
